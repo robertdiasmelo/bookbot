fa0668cc2af1596d4395236f7a02a09245976c0b
robertdiasmelo <crobertdmelo@gmail.com>
2025-01-26 23:54:11 -0300

    add report function feature

---
+++ main.py
@@ -0,0 +1,58 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_number_words(text)
+    num_characters = get_number_characters(text)
+
+    print_report(book_path, num_words, num_characters)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+def get_number_words(text):
+    return len(text.split())
+
+
+def get_number_characters(text):
+    lowered_text = text.lower()
+    characters_number = {}
+    for character in lowered_text:
+        if character in characters_number:
+            characters_number[character] += 1
+        else:
+            characters_number[character] = 1
+    return characters_number
+
+
+def print_report(path, num_words, num_characters):
+    print(f"--- Begin report of {path} ---")
+    print(f"{num_words} words found in the document")
+    print()
+    print()
+
+    number_characters_list = chars_dict_to_sorted_list(num_characters)
+    
+    for character_num_dict in number_characters_list:
+        if character_num_dict["character"].isalpha():
+            print(f"The '{character_num_dict["character"]}' character was found {character_num_dict["num"]} times")
+        else:
+            continue
+    print("--- End report ---")
+
+
+def chars_dict_to_sorted_list(my_dict):
+    my_list = []
+    for letter, number in my_dict.items():
+        my_list.append({"character": letter, "num": number})
+    my_list.sort(reverse=True, key=sort_on)
+    return my_list
+
+
+def sort_on(dict):
+    return dict["num"]
+
+
+main()
